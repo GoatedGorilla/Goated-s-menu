8e1f2ad2ff536362bd957e18b4659930293051ed
GoatedGorilla <luke.mike.wilson@gmail.com>
2025-06-12 18:46:03 -0500

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,7 +3,7 @@
     public class PluginInfo
     {
         public const string GUID = "org.iidk.gorillatag.iimenu";
-        public const string Name = "ii's Stupid Menu";
+        public const string Name = "Goeated menu";
         public const string Description = "Created by @goldentrophy with love <3";
         public const string BuildTimestamp = "2025-06-12T15:04:21Z";
         public const string Version = "6.2.1";
